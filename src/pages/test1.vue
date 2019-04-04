d240d50e01c4e49800dc0b650aa708fcf5e7fd85
wenjun.lv <wenjun.lv@wenjunlvdeMacBook-Air.local>
2019-04-04 18:27:31 +0800

    子路由测试

--- src/pages/test1.vue
+++ src/pages/test1.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="test1">
-        <el-button>test1-1</el-button>
-        <el-button>test1-2</el-button>
+        <el-button @click="$router.push('test1-1')">test1-1</el-button>
+        <el-button @click="$router.push('test1-2')">test1-2</el-button>
+        <div>
+            <router-view></router-view>
+        </div>
     </div>
 </template>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,23 @@
 import test1 from '../pages/test1'
 import test2 from '../pages/test2'
 import test3 from '../pages/test3'
+import test1A from '../pages/test1.1'
+import test1B from '../pages/test1.2'
+
 const routes=[
     {
         path:'/test1',
-        component:test1
+        component:test1,
+        redirect:'/test1/test1-1',
+        children:[
+            {
+                path:'test1-1',
+                component:test1A
+            },{
+                path:'test1-2',
+                component:test1B
+            }
+        ]
     },
     {
         path:'/test2',
